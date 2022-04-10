4c69daee9195d4c6f1a6548aa3be4e26917b47de
mercyakuku <mercyakuku24@gmail.com>
2022-04-10 22:37:17 +0300

    Add My Quotes and Quotes Form(h2)

---
+++ src/app/quotes.ts
@@ -0,0 +1,12 @@
+export class Quotes {
+    id: number;
+    quote: string;
+    upvotes: number;
+    downvotes: number;
+    constructor(id:number, quote:string, upvotes:number, downvotes:number){
+      this.id = id;
+      this.quote = quote;
+      this.upvotes = upvotes;
+      this.downvotes = downvotes;
+    }
+}
